438de79dcc4f0fc30d8a6948b5be867185f4a5dc
FLOW <florianmart2004@gmail.com>
2025-02-12 16:50:51 +0100

    sommaire

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ window.addEventListener('scroll', () => {
     const title = document.querySelector('.title');
     const subtitle = document.querySelector('.subtitle');
     const socialMedia = document.querySelector('.contact');
+    const sommaire = document.querySelector('#sommaire');
     const guirlandes = document.querySelector('.guirlandes');
 
     // Vitesse différente pour le titre et le sous-titre
@@ -13,7 +14,15 @@ window.addEventListener('scroll', () => {
     subtitle.style.transform = `translateY(${scrollY * 0.5}px)`;
     socialMedia.style.transform = `translateY(${scrollY * 0.3}px)`;
     guirlandes.style.backgroundPositionY = `${scrollY * -0.5}px`;
-    
+
+
+    const threshold = 500; // Définissez le seuil ici
+    if (scrollY > threshold) {
+        sommaire.style.transform = 'translateY(0%)';
+    } else {
+        sommaire.style.transform = 'translateY(-100%)';
+    }
+
     const opacity = Math.max(1 - scrollY / 700, 0);
     socialMedia.style.opacity = opacity;
     
